9e856b667c5f1369db2a8f3cef17d05587a9db38
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-05-22 12:45:58 -0500

    config js prettier

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,28 @@
-angular.module("envconfig", [])
-.constant("dev", {"baseUrl":"http://localhost:1337","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"dev"})
-.constant("sandbox", {"baseUrl":"http://166.78.47.146:8080","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"sandbox"})
-.constant("demo", {"baseUrl":"http://demo-actual-api.herokuapp.com","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"demo"})
-.constant("production", {"baseUrl":"http://api.miactual.com","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"production"})
-.constant("ENV", {"baseUrl":"http://localhost:1337","cdnUrl":"https://d116li125og699.cloudfront.net","tokenPrefix":"dev","name":"dev"});
+angular
+  .module('envconfig', [])
+  .constant('dev', {
+    baseUrl: 'http://localhost:1337',
+    cdnUrl: 'https://d116li125og699.cloudfront.net',
+    tokenPrefix: 'dev',
+  })
+  .constant('sandbox', {
+    baseUrl: 'http://166.78.47.146:8080',
+    cdnUrl: 'https://d116li125og699.cloudfront.net',
+    tokenPrefix: 'sandbox',
+  })
+  .constant('demo', {
+    baseUrl: 'http://demo-actual-api.herokuapp.com',
+    cdnUrl: 'https://d116li125og699.cloudfront.net',
+    tokenPrefix: 'demo',
+  })
+  .constant('production', {
+    baseUrl: 'http://api.miactual.com',
+    cdnUrl: 'https://d116li125og699.cloudfront.net',
+    tokenPrefix: 'production',
+  })
+  .constant('ENV', {
+    baseUrl: 'http://localhost:1337',
+    cdnUrl: 'https://d116li125og699.cloudfront.net',
+    tokenPrefix: 'dev',
+    name: 'dev',
+  });
